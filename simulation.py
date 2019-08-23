7d8464ccd3b76efe9939600dec9dc3671c4a0137
unknown <a.kalkauskas@gmail.com>
2019-08-23 11:11:28 +0100

    adding bounds of the samples

--- simulation.py
+++ simulation.py
@@ -522,7 +522,7 @@ for i in range(num_trees*(job_index-1), num_trees*job_index):
             os.system('treeannotator -burnin '+str(burnin)+' "output/beast/no_sampling/beast_output/beast'+str(i)+'.trees.txt" "output/beast/no_sampling/annotated_trees/beast'+str(i)+'.tree.txt"')
         
     
-    run_sample_analysis= True
+    run_sample_analysis= False
     if run_sample_analysis:
         for output_index in range(1, num_sampling+1):
             if linux:
