e199da00fbfa743b469fdc87e71844f94f9ed574
unknown <a.kalkauskas@gmail.com>
2019-09-03 09:50:20 +0100

    add convenient root location output

--- simcorrection.py
+++ simcorrection.py
@@ -27,11 +27,11 @@ job_index = args.job_index
 
 
 
-num_tips = 50
-sample_size = 5
-other_sample_size = 5
+num_tips = 500
+sample_size = 50
+other_sample_size = 50
 sigma = 1
-seq_len = 100 # 10000
+seq_len = 10000 # 10000
 
 #num_simulations = 1
 
@@ -48,16 +48,16 @@ for i in range(num_trees*(job_index-1), num_trees*job_index):
     
     
     sampled_t = sampling.sample_biased_extreme(tree, sample_ratio = sample_size/num_tips, dimension = 2)    
-    d = dendropy.model.discrete.hky85_chars(kappa=3, mutation_rate=0.1, seq_len=seq_len, tree_model=sampled_t, retain_sequences_on_tree=False)    
-    beastxmlwriter.write_BEAST_xml_corrected(tree, sampled_t, d, i=i,  mcmc=100000, log_every=1000, beast_input_string ="output/c_beast/beast_input/beast", beast_output_string="output/c_beast/beast_output/beast", other_sample_size=other_sample_size, seq_len=seq_len)
+    d = dendropy.model.discrete.hky85_chars(kappa=3, mutation_rate=0.05, seq_len=seq_len, tree_model=sampled_t, retain_sequences_on_tree=False)    
+    beastxmlwriter.write_BEAST_xml_corrected(tree, sampled_t, d, i=i,  mcmc=1000000, log_every=1000, beast_input_string ="output/c_beast/beast_input/beast", beast_output_string="output/c_beast/beast_output/beast", other_sample_size=other_sample_size, seq_len=seq_len)
     
-    beastxmlwriter.write_BEAST_xml(sampled_t, i=i, dimension=2, mcmc=100000, log_every=1000, beast_input_string ="output/c_beast/beast_input/nbeast", beast_output_string="output/c_beast/beast_output/nbeast")
+    beastxmlwriter.write_BEAST_xml(sampled_t, i=i, dimension=2, mcmc=1000000, log_every=1000, beast_input_string ="output/c_beast/beast_input/nbeast", beast_output_string="output/c_beast/beast_output/nbeast")
     
     
     os.system('beast -overwrite -seed 123456795 "output/c_beast/beast_input/beast'+str(i)+'.xml"')
     os.system('beast -overwrite -seed 123456795 "output/c_beast/beast_input/nbeast'+str(i)+'.xml"')
     
-    file = open("output/actual_root"+str(i)+".txt", "w")
+    file = open("output/root_data/actual_root"+str(i)+".txt", "w")
     file.write(str(sampled_t.seed_node.X)+'\n')
     file.write(str(sampled_t.seed_node.Y)+'\n')
     file.close()
@@ -69,7 +69,7 @@ for i in range(num_trees*(job_index-1), num_trees*job_index):
         sample_taxon_labels.append(leaf.taxon.label)
     
     
-    file = open("output/observed_roots"+str(i)+".txt", "w")    
+    file = open("output/root_data/observed_roots"+str(i)+".txt", "w")    
     treelist = dendropy.TreeList.get(path="output/c_beast/beast_output/beast"+str(i)+".trees.txt", extract_comment_metadata=True, schema="nexus")
     for single_tree in treelist:
         
@@ -83,7 +83,7 @@ for i in range(num_trees*(job_index-1), num_trees*job_index):
     
     
     
-    file = open("output/old_observed_roots"+str(i)+".txt", "w")    
+    file = open("output/root_data/old_observed_roots"+str(i)+".txt", "w")    
     treelist = dendropy.TreeList.get(path="output/c_beast/beast_output/nbeast"+str(i)+".trees.txt", extract_comment_metadata=True, schema="nexus")
     for single_tree in treelist:
         file.write(single_tree.seed_node.annotations.require_value("location")[0]+"\t"+single_tree.seed_node.annotations.require_value("location")[1]+'\n')
