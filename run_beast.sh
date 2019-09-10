f0b2451efa33b9edcfebcb913f2c9d0aeb29bf90
AntanasKal <a.kalkauskas@gmail.com>
2019-09-10 16:42:56 +0100

    adding sh file

--- run_beast.sh
+++ run_beast.sh
@@ -1,3 +1,3 @@
-for i in $(seq 1 100)
-do bsub -o console_output/out_"$i".txt -e console_output/err_"$i".txt python simulation.py -jobi "$i" -N 1 -dims 2 -treetype yule -ntips 1000 --linux -mcmc 5000
+for i in $(seq 0 99)
+do bsub -o console_output/out_"$i".txt -e console_output/err_"$i".txt python simulation.py -jobi "$i" -N 1 -dims 2 -treetype nuc -ntipspp 100 -nps 20 --linux -mcmc 10000
 done
