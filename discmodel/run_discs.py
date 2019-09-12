987cbb1bf2bb1307deea76384a23a53582cac941
AntanasKal <a.kalkauskas@gmail.com>
2019-09-12 16:41:55 +0100

    reorganising stuff

--- discmodel/run_discs.py
+++
@@ -1,4 +0,0 @@
-bgadd -L 200 /discs
-for i in $(seq 107 107)
-do bsub -g /discs -o console_output2/out_"$i".txt -e console_output2/err_"$i".txt python disc2.py -jobi "$i" -N 1
-done
--- discmodel/run_simulations.sh
+++
@@ -1,3 +0,0 @@
-for i in $(seq 1 1000)
-do bsub -o console_output/out_"$i".txt -e console_output/err_"$i".txt python disc_simulation.py -jobi "$i" -N 10
-done
