d176be7fd7bc133c9e8a69f54b1c88c26be734d5
unknown <a.kalkauskas@gmail.com>
2019-09-12 16:41:04 +0100

    changing disc.py

--- discmodel/discs.py
+++ discmodel/discs.py
@@ -56,8 +56,8 @@ if not os.path.exists("output/phyrex/LV/phyrex_output"):
 	os.makedirs("output/phyrex/LV/phyrex_output")
 if not os.path.exists("output/phyrex/LV/phyrex_input"):
 	os.makedirs("output/phyrex/LV/phyrex_input")
-if not os.path.exists("output/root_locations"):
-	os.makedirs("output/root_locations")
+if not os.path.exists("output/root_data"):
+	os.makedirs("output/root_data")
 
 for index in range(job_index*num_simulations, (job_index+1)*num_simulations):
 	L = 100
@@ -78,18 +78,14 @@ for index in range(job_index*num_simulations, (job_index+1)*num_simulations):
 			x[i] = (random.uniform(45, 55))%L
 			y[i] = (random.uniform(45, 55))%L
 			a.append((x[i], y[i]))
-	#a=[None, (0,0), (0, 1)]
+
 	sim.sample = a
-	#print(a)
+
 	rad = 0.1    
-	#lambda1 = 5000000
-	#mu = L**2/(lambda1*4*math.pi*rad**4)
-	#mu = L**2/(lambda1*rad**4)
-	#mu= L**2/(lambda1*rad**4*math.pi*(1-(128/(45*math.pi))**2))
+
 	mu=0.1
 	lambda1=2*L**2/(mu*rad**4*math.pi)
-	#print(index)
-	#print("lambda = "+str(lambda1))
+
 	sim.event_classes = [ercs.DiscEventClass(r = rad, u = mu, rate = lambda1)]
 	''' All individuals within distance r of the centre of an event
 	    have probability u of dying in the event 
@@ -101,23 +97,15 @@ for index in range(job_index*num_simulations, (job_index+1)*num_simulations):
 	
 	pi, tau = sim.get_history()
 	al = sim.get_population()[0][0]
-	#print(sim.get_population())
-	#file.write(str(tau[0][3])+"\n")
-	#print("mu = "+str(mu))
-	#print("rad = "+str(rad))
 
-	file = open("output/root_locations/location"+str(index)+".txt", "w")
-	file.write(str(al[0])+" "+str(al[1]))
+
+	file = open("output/root_data/actual_root"+str(index)+".txt", "w")
+	file.write(str(al[0])+"\n"+str(al[1]))
 	file.close()
-	#print(str(pi))
-	#print(str(tau))
+
 	tree_newick = newick.convert_to_newick(pi, tau, True)
 	tree = dendropy.Tree.get(data=tree_newick, schema="newick")
-	#for node in tree.preorder_node_iter():
-	#	if node.edge_length == None:
-	#		node.edge_length = 0
-	#		print("asdf")
-	#		print(node.edge.length)
+
 	tree = treegenerator.calculate_times(tree)
 	tree = ultrametrize(tree)
 	for leaf_index in range(1,n+1):
@@ -137,5 +125,3 @@ for index in range(job_index*num_simulations, (job_index+1)*num_simulations):
 	beastxmlwriter.write_BEAST_xml(tree, index, dimension=2, mcmc=10000, log_every=10, beast_input_string="output/beast/LV/beast_input/beast", beast_output_string="output/beast/LV/beast_output/beast")
 	phyrexxmlwriter.write_phyrex_input(tree, index, input_string="output/phyrex/LV/phyrex_input/" , output_string="output/phyrex/LV/phyrex_output/", bound=L)
 		
-	#os.system('beast -overwrite -seed 123456795 "output/beast/LV/beast_input/beast'+str(index)+'.xml"') 
-
